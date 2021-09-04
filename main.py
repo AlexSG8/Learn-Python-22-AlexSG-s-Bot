e1a9f532b3cbfa3ab825754e90c90f5f27446400
Alex Gatsenko <jocker.fun@gmail.com>
2021-09-04 23:40:54 +0400

    Добавленв игра в числа

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+from random import randint
 
 logging.basicConfig(filename='bot.log',
                     level=logging.INFO)
@@ -35,11 +36,40 @@ def talk_to_me(update, context):
     update.message.reply_text(user_text)
 
 
+def play_random_numbers(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+
+    if bot_number == user_number:
+        message = f'{bot_number} = {user_number}. Ничья.'
+    elif bot_number > user_number:
+        message = f'{bot_number} > {user_number}. Ты проиграл!'
+    else:
+        message = f'{bot_number} < {user_number}. Ты выиграл!'
+
+    return message
+
+
+def guess_number(update, context):
+
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_numbers(user_number)
+        except (TypeError, ValueError):
+            message = "Введите целое число"
+    else:
+        message = "Введите целое число"
+
+    update.message.reply_text(message)
+
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("guess", guess_number))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
